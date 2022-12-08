106bc243895a62af699a9450a91559a243382be6
Cyril Caparanga <cyrilcaps@gmail.com>
2022-12-08 17:36:00 -0500

    Add exception handling worker

--- worker.py
+++ worker.py
@@ -1,3 +1,4 @@
+import traceback
 import socket
 import threading
 import time
@@ -35,50 +36,56 @@ if __name__ == '__main__':
     HOST = args.host
     PORT = args.port
     #Creating the socket to connect to the manager
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        def heartbeat(): #Sends and heartbeat every 3 seconds
-            msg = {"type": "status"}
-            while True:
-                if active:
-                    msg["status"] = "Processing Request"
-                    payload = json.dumps(msg)
-                    s.sendall(payload.encode())
-                else:
-                    msg["status"] = "Idle"
-                    payload = json.dumps(msg)
-                    s.sendall(payload.encode())
-                time.sleep(3)
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                def heartbeat(): #Sends and heartbeat every 3 seconds
+                    msg = {"type": "status"}
+                    while True:
+                        if active:
+                            msg["status"] = "Processing Request"
+                            payload = json.dumps(msg)
+                            s.sendall(payload.encode())
+                        else:
+                            msg["status"] = "Idle"
+                            payload = json.dumps(msg)
+                            s.sendall(payload.encode())
+                        time.sleep(3)
 
-        s.connect((HOST, PORT))
-        t = threading.Thread(target=heartbeat) #Create the heartbeat thread
-        t.start()
-        while True: 
-            received = s.recv(BUFSIZE) #Receive the work
-            if received: #Check if received work
-                msg = {"type":"status"}
-                data = json.loads(received)
-                active = True
-                tobeHashed = data["range"][0]
-                #Calculate Hash until find or reach the end
-                while tobeHashed != data["range"][1]:
-                    if data["hash"] == util.md5(tobeHashed):
-                        msg["status"] = "Hash Found"
-                        msg["pass"] = tobeHashed
-                        payload = json.dumps(msg)
-                        s.sendall(payload.encode())
-                        break
-                    tobeHashed = increment_str(tobeHashed)
-                if tobeHashed == data["range"][1]:
-                    if data["hash"] == util.md5(tobeHashed):
-                        msg["status"] = "Hash Found"
-                        msg["hash"] = data["hash"]
-                        msg["pass"] = tobeHashed
-                        payload = json.dumps(msg)
-                        s.sendall(payload.encode())
-                    else:
-                        msg["status"] = "Hash Not Found"
-                        msg["hash"] = data["hash"]
-                        payload = json.dumps(msg)
-                        s.sendall(payload.encode())
-                    
-            active = False
+                s.connect((HOST, PORT))
+                t = threading.Thread(target=heartbeat) #Create the heartbeat thread
+                t.start()
+                while True:
+                    received = s.recv(BUFSIZE) #Receive the work
+                    if received: #Check if received work
+                        msg = {"type":"status"}
+                        data = json.loads(received)
+                        active = True
+                        tobeHashed = data["range"][0]
+                        #Calculate Hash until find or reach the end
+                        while tobeHashed != data["range"][1]:
+                            if data["hash"] == util.md5(tobeHashed):
+                                msg["status"] = "Hash Found"
+                                msg["pass"] = tobeHashed
+                                payload = json.dumps(msg)
+                                s.sendall(payload.encode())
+                                break
+                            tobeHashed = increment_str(tobeHashed)
+                        if tobeHashed == data["range"][1]:
+                            if data["hash"] == util.md5(tobeHashed):
+                                msg["status"] = "Hash Found"
+                                msg["hash"] = data["hash"]
+                                msg["pass"] = tobeHashed
+                                payload = json.dumps(msg)
+                                s.sendall(payload.encode())
+                            else:
+                                msg["status"] = "Hash Not Found"
+                                msg["hash"] = data["hash"]
+                                payload = json.dumps(msg)
+                                s.sendall(payload.encode())
+
+                    active = False
+        except Exception as e:
+            print(traceback.format_exc())
+        print(f"Reconnecting to {HOST}:{PORT}...")
+        time.sleep(2)
