bb7178680e7e0b3f829b41a5315edacdbc3cce9f
Akshaya Bali <akshayabali@gmail.com>
2022-12-07 22:41:15 -0500

    Update

--- manager.py
+++ manager.py
@@ -88,6 +88,8 @@ class Manager:
         self.hash = ""  # Hash to be processed, blank if idle
         self.lower = -1  # Lower range of processing request
         self.upper = -1  # Higher range of processing request
+        self.clower = -1
+        self.cupper = -1
         self.queue = {
             0 : [0, 0]
         }
@@ -110,16 +112,19 @@ class Manager:
     def convert_to_int(self, s: str) -> int:
         #Takes a base 52 character string and converts to base 10 integer
         val = 0
+        idx = 0
         for i in range(4, -1, -1):
-            val = val + Alphabet[s[i]].value * (52**i)
+            val = val + Alphabet[s[i]].value * (52**idx)
+            idx = idx + 1
         return val
 
-    def give_work(self, connection, ID, skip=False):
+    def give_work_worker(self, connection, ID, skip=False):
         try:
             while True:
                 if not skip:
                     if (time.time() - self.worker_timer[ID - 1]) > 10:
                         print(self.worker_timer[ID - 1],time.time()," Worker Died")
+
                         connection.close()
                         break
                 received = connection.recv(1024)
@@ -131,10 +136,12 @@ class Manager:
                         if self.status[ID - 1] == "Idle" and self.hash != "":
                             #Idle and Hash Not Found to be given same function
                             self.lock.acquire()
-                            self.lower = self.upper + 1
-                            self.upper = self.lower + 1000
-                            self.queue[ID] = [self.lower, self.upper]
-                            sending_data = {"hash" : self.hash,"type": "ordered", "range" : [self.convert_to_string(self.lower), self.convert_to_string(self.upper)]}
+                            self.clower = self.lower
+                            self.cupper = self.lower + 1000
+                            if(self.cupper > self.upper):
+                                self.cupper = self.upper
+                            self.queue[ID] = [self.clower, self.cupper]
+                            sending_data = {"hash" : self.hash,"type": "ordered", "range" : [self.convert_to_string(self.clower), self.convert_to_string(self.cupper)]}
                             self.lock.release()
                             sending_data = json.dumps(sending_data)
                             print(ID-1," Sending: ",sending_data)
@@ -150,60 +157,92 @@ class Manager:
                             self.upper = ""
                             self.lock.release()
                         elif self.status[ID - 1].startswith("Hash Not Found"):
-                            time_taken = time.time() - self.req_timer[ID - 1]
-                            rang = self.queue[ID][1] - self.queue[ID][0]
-                            hash_rate = rang // time_taken
-                            work = hash_rate * 10
-                            self.lock.acquire()
-                            self.lower = self.upper + 1
-                            self.upper = self.lower + work
-                            self.queue[ID] = [self.lower, self.upper]
-                            sending_data = {"hash": self.hash,"type": "ordered", "range" : [self.convert_to_string(self.lower), self.convert_to_string(self.upper)]}
-                            self.lock.release()
-                            sending_data = json.dumps(sending_data)
-                            print("Sending to ",ID-1 , " : ",sending_data, " Hash Rate : ",hash_rate)
-                            connection.sendall(sending_data.encode())
-                            self.req_timer[ID - 1] = time.time()
+                            if(self.cupper >= self.upper):
+                                with self.lock:
+                                    msg = {"type":"status"}
+                                    msg["status"] = "Hash Not Found"
+                                    msg["hash"] = self.hash
+                                    sending_data = json.dumps(msg)
+                                    self.master["master_socket"].sendall(sending_data.encode())
+                                    self.lhash = self.hash
+                                    self.hash = ""
+                                    self.lower = ""
+                                    self.upper = ""
+                            else:
+                                time_taken = time.time() - self.req_timer[ID - 1]
+                                rang = self.queue[ID][1] - self.queue[ID][0]
+                                hash_rate = rang // time_taken
+                                work = hash_rate * 10
+                                self.lock.acquire()
+                                self.clower = self.cupper + 1
+                                self.cupper = self.clower + work
+                                if(self.cupper > self.upper):
+                                    self.cupper = self.upper
+                                self.queue[ID] = [self.clower, self.cupper]
+                                sending_data = {"hash": self.hash,"type": "ordered", "range" : [self.convert_to_string(self.clower), self.convert_to_string(self.cupper)]}
+                                self.lock.release()
+                                sending_data = json.dumps(sending_data)
+                                print("Sending to ",ID-1 , " : ",sending_data, " Hash Rate : ",hash_rate)
+                                connection.sendall(sending_data.encode())
+                                self.req_timer[ID - 1] = time.time()
                         self.worker_timer[ID - 1] = time.time()
-                elif message["type"] == "ordered":
-                    if self.hash == "":
-                        with self.lock:
-                            self.hash = message["hash"]
-                            self.lower = self.convert_to_int(message["range"][0])
-                            self.upper = self.convert_to_int(message["range"][1])
-                        while True:
-                            with self.lock:
-                                if self.found != "":
-                                    msg = {"type": "status"}
-                                    msg["status"] = "Hash Found"
-                                    msg["hash"] = self.lhash
-                                    msg["pass"] = self.found
-                                    payload = json.dumps(msg)
-                                    connection.sendall(payload.encode())
-                                    self.found = ""
-                                    connection.close()
-                                    break
-                            time.sleep(10)
-                    else:
-                        with self.lock:
-                            msg = {"type": "status", "status": "Busy"}
-                            sending_data = json.dumps(msg)
-                            connection.sendall(sending_data.encode())
         except Exception as e:
             print(e)
             traceback.print_exc()
             connection.close()
             pass
 
+    def give_work_master(self, connection, ID, skip=False):
+        try:
+            while True:
+                received = connection.recv(1024)
+                print(ID-1, " Received from master: ", received)
+                message = json.loads(received.decode("utf-8"))
+                if message["type"] == "ordered":
+                    if self.hash == "":
+                        with self.lock:
+                            self.hash = message["hash"]
+                            self.lower = self.convert_to_int(message["range"][0])
+                            self.upper = self.convert_to_int(message["range"][1])
+                        # while True:
+                        #     with self.lock:
+                        #         if self.found != "":
+                        #             msg = {"type": "status"}
+                        #             msg["status"] = "Hash Found"
+                        #             msg["hash"] = self.lhash
+                        #             msg["pass"] = self.found
+                        #             payload = json.dumps(msg)
+                        #             print("Sending to master:", msg)
+                        #             connection.sendall(payload.encode())
+                        #             self.found = ""
+                        #             connection.close()
+                        #             break
+                        #     time.sleep(10)
+                    else:
+                        with self.lock:
+                            msg = {"type": "status", "status": "Busy"}
+                            sending_data = json.dumps(msg)
+                            print("Sending to master:", msg)
+                            connection.sendall(sending_data.encode())
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+            connection.close()
+
     def send_heartbeat(self):
         msg = {"type":"status"}
         while True:
+            n_worker = 0
             if(len(self.status) > 0):
                 msg["status"] = "Processing Request"
                 for i in range(len(self.status)):
                     if(self.status[i] == "Idle"):
                         msg["status"] = "Idle"
+                    if (time.time() - self.worker_timer[i]) < 10:
+                        n_worker = n_worker + 1
             else:
+                msg["status"] = "No Worker Available"
+            if n_worker == 0: #>= len(self.status):
                 msg["status"] = "No Worker Available"
             payload = json.dumps(msg)
             self.master["master_socket"].sendall(payload.encode())
@@ -230,7 +269,7 @@ class Manager:
             print("Connected with worker " + ip + ":" + port)
             self.worker_timer.insert(i-1, time.time())
             self.children.append(i)
-            t = threading.Thread(target = self.give_work, args= (connection, i, ))
+            t = threading.Thread(target = self.give_work_worker, args= (connection, i, ))
             t.start()
 
     def connect_to_master(self):
@@ -238,8 +277,10 @@ class Manager:
         masterPort = int(self.master["master_port"])
         self.master["master_socket"].connect((self.master["master_ip"], masterPort))
         print("Connection sucessfull to Master")
-        t = threading.Thread(target=self.send_heartbeat)
-        t.start()
+        t1 = threading.Thread(target=self.send_heartbeat)
+        t2 = threading.Thread(target=self.give_work_master, args= (self.master["master_socket"], 0, True))
+        t1.start()
+        t2.start()
 
     def start(self):
         master_client = threading.Thread(target=self.connect_to_master)
